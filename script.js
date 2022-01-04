243e4e4278e3931aa11f0461a3fc7ae124a5fa88
Your Name <business.joshuac@gmail.com>
2022-01-04 11:07:06 -0600

    js basic pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+// how to play
+// there is a random number chosen between 1-100
+// the player and computer (or second player?) are playing against each other to guess the number
+//  (optional) the players have a limited amount of tries (determined by difficulty level) to guess the number
+// if the player guesses the numberbefore the computer does, than the player gets a point
+// if the player and the computer both guess the number in the same amount of tries than none get points
+// if the computer guesses before the player does, the computer gets a point
+// whoever gets five points first, wins 
+// ask if the player wants to play again
